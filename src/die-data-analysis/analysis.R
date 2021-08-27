b540a53b5ef8ccf4a2d9f118314749cfeec0ede6
Sejin Kim <kim3@kenyon.edu>
2021-08-27 11:45:35 -0400

    Add wilcoxon test

--- src/die-data-analysis/analysis.R
+++ src/die-data-analysis/analysis.R
@@ -3,6 +3,7 @@
 
 # Import libraries
 library(dplyr)
+library(ggpubr)
 
 # Import data
 dataset1 <- readRDS(url("https://github.com/kim3-sudo/die-data-analysis/blob/main/data/die_dataset_1.rds?raw=true"))
@@ -10,14 +11,17 @@ dataset2 <- readRDS(url("https://github.com/kim3-sudo/die-data-analysis/blob/mai
 View(dataset1)
 View(dataset2)
 
-hist(dataset1$value, breaks=seq.int(1, 6, 1), xlab = "Value", ylab = "Frequency", main = "Dataset 1 Values")
-hist(dataset2$value, breaks=seq.int(1, 6, 1), xlab = "Value", ylab = "Frequency", main = "Dataset 2 Values")
+dataset1 <- as.vector(dataset1[,1])
+dataset2 <- as.vector(dataset2[,1])
+
+hist(dataset1, breaks=seq.int(1, 6, 1), xlab = "Value", ylab = "Frequency", main = "Dataset 1 Values")
+hist(dataset2, breaks=seq.int(1, 6, 1), xlab = "Value", ylab = "Frequency", main = "Dataset 2 Values")
 
 
 # Combine the datasets into one
 diedata <- data.frame(
-  group = rep(c("set1", "set2"), each = 25),
-  weight = c(dataset1, dataset2)
+  group = rep(c("die1", "die2"), each = 25),
+  value = c(dataset1, dataset2)
 )
 
 # EDA
@@ -28,6 +32,9 @@ group_by(diedata, group) %>%
     IQR = IQR(value, na.rm = TRUE)
   )
 
+
 # Wilcoxon Rank Sum Test
 ## H_0 = The two datasets are derived from the same die
 ## H_1 = There is some force acting on one of the die
+wtest <- wilcox.test(value ~ group, data = diedata, exact = FALSE, alternative="less")
+wtest
